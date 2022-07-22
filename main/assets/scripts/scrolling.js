2626a862905d4e06870530fbac73436542257f38
luckoff <neluckoff@gmail.com>
2022-07-22 18:54:32 +0300

    transparent header, icons added and a picture between pages implemented

--- main/assets/scripts/scrolling.js
+++ main/assets/scripts/scrolling.js
@@ -1,12 +1,14 @@
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
-    anchor.addEventListener("click", function (e) {
-        event.preventDefault()
-        const blockID = anchor.getAttribute('href')
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
         })
     })
 }
